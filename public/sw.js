77ee4a23a010c60ac8ad10c944bfe2fde2bada2e
achoblet33-web <a.choblet33@gmail.com>
2026-09-14 11:35:01 -0400

    Renouveler le cache PWA pour la visionneuse

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'danz-shell-v21'
+const CACHE_NAME = 'danz-shell-v22'
 const PRIVATE_MEDIA_CACHE = 'danz-private-thumbs-v2'
 const APP_ROOT = '/danz/'
 const OFFICIAL_LOGO = '/danz/image001-1.png?v=official-image001-1-20260914'
